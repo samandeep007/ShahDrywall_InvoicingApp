c3a6d9123d848d5cb07d032634b6b474cce63bf2
samandeep007 <hacktechnicals@gmail.com>
2024-09-21 16:09:08 -0600

    Update 6.0

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -133,7 +133,8 @@ export default function Invoice() {
                         stack: [
                             { text: "Shah Drywall Ltd.", style: "companyName" },
                             { text: "6014 Saddlehorn Dr NE, T3J 4M4", style: "companyAddress" },
-                            { text: "GST Number: 123456789", style: "companyAddress" },
+                            { text: "GST Number: 838419281RT0001", style: "companyAddress" },
+                            { text: "WCB Number: 956188", style: "companyAddress" },
                         ],
                         alignment: "left",
                     },
